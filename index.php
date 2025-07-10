44d23c1001544c2e720aa4d65553b9f0db118a53
pane <carpincho.daniel.pane@gmail.com>
2025-07-10 11:25:22 -0300

    guardo cambios

--- index.php
+++ index.php
@@ -3,20 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>dealership</title>
 </head>
 <body>
     <header class="header">
-        <div class="logo"><img src="app.logomakr.com/4sQFiC" alt=""></div>
-        <div class="new"><a href="new.php"></a></div>
-        <div class="used"><a href="used.php"></a></div>
-        <div class="allvehicles"><a href="allvehicles.php"></a></div>
-        <div class="certified"><a href="certified.php"></a></div>
-        <div class="finance"><a href="finance.php"></a></div>
-        <div class="service"><a href="service.php"></a></div>
-        <div class="aboutUs"><a href="aboutUs.php"></a></div>
-        <div class="Login"><a href="login.php"></a></div>
-        <div class="register"><a href="register.php"></a></div>
+        <div ><img src="logo.png" alt="" class="logo"></div>
+        <div class="new"><a href="new.php">New</a></div>
+        <div class="used"><a href="used.php">Used</a></div>
+        <div class="allvehicles"><a href="allvehicles.php">All Vehicles</a></div>
+        <div class="certified"><a href="certified.php">Certified</a></div>
+        <div class="finance"><a href="finance.php">Finance</a></div>
+        <div class="service"><a href="service.php">Service</a></div>
+        <div class="aboutUs"><a href="aboutUs.php">About Us</a></div>
+        <div class="Login"><a href="login.php">login</a></div>
+        <div class="register"><a href="register.php">Register</a></div>
     </header>
 
     <section class="hero-banner">
