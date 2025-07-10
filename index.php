24f734dbaa643a35d71d355ab99309ce0465884e
pane <carpincho.daniel.pane@gmail.com>
2025-07-10 10:16:36 -0300

    mejora

--- index.php
+++ index.php
@@ -6,6 +6,29 @@
     <title>dealership</title>
 </head>
 <body>
-    
+    <header class="header">
+        <div class="logo"><img src="app.logomakr.com/4sQFiC" alt=""></div>
+        <div class="new"><a href="new.php"></a></div>
+        <div class="used"><a href="used.php"></a></div>
+        <div class="allvehicles"><a href="allvehicles.php"></a></div>
+        <div class="certified"><a href="certified.php"></a></div>
+        <div class="finance"><a href="finance.php"></a></div>
+        <div class="service"><a href="service.php"></a></div>
+        <div class="aboutUs"><a href="aboutUs.php"></a></div>
+        <div class="Login"><a href="login.php"></a></div>
+        <div class="register"><a href="register.php"></a></div>
+    </header>
+
+    <section class="hero-banner">
+        <div class="banner"></div>
+    </section>
+
+    <section class="feature vehicles">
+    <div class="car1"></div>
+    <div class="car2"></div>
+    <div class="car3"></div>
+    </section>
+
+    <section class="contac us"></section>
 </body>
 </html>
